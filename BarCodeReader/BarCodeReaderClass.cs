26da206477c18c83486326009de9499630ca9345
andrew <vasilyev_a@rakurs.com>
2023-10-11 15:38:51 +0300

    Реализация класса работы со сканером штрих-кодов
    Исключение отслеживания ненужных файлов

---
+++ BarCodeReader/BarCodeReaderClass.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+
+namespace BarCodeReader
+{
+    public class BarCodeReaderClass
+    {
+        public SerialPort Port { get; set; }
+
+        public string BarCode { get; set; }
+
+        public BarCodeReaderClass()
+        {
+
+        }
+
+        public BarCodeReaderClass(SerialPort port)
+        {
+            Port = port;
+        }
+
+        public async Task StartReadAsync()
+        {
+            BarCode = "";
+            Port.DiscardInBuffer();
+            Port.DataReceived += DataReceivedHandler;
+            await CyclePortRead();
+        }
+        private async Task CyclePortRead()
+        {
+            while (BarCode.Length == 0)
+            {
+                await Task.Delay(500);
+            };
+        }
+
+        private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            int totalBytes = Port.BytesToRead;
+
+            if (totalBytes > 0)
+            {
+                byte[] buffer = new byte[totalBytes];
+                Port.Read(buffer, 0, totalBytes);
+
+                Encoding encoding = Encoding.GetEncoding("iso-8859-1");
+                string barcodeText = encoding.GetString(buffer);
+
+                // Trim /r
+                BarCode = barcodeText.Substring(0, 12);
+                BarCode = "0" + BarCode;
+            }
+        }
+
+
+    }
+}
---
+++ BarCodeReader/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные со сборкой.
+[assembly: AssemblyTitle("BarCodeReader")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BarCodeReader")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("43df657b-f36e-40dc-bbd6-290dcb3da5c4")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
