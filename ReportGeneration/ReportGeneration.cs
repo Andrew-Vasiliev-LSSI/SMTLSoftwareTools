f4f4c952623b64b6e335f56fba10d5ebf8eed519
andrew <vasilyev_a@rakurs.com>
2024-12-26 10:46:39 +0300

    Модернизация класса ReportGeneration
    Отказ от директивы using.
    Класс package сделан глобальным.
    Создание экземпляра класса package перенесено в конструктор.
    Добавленно свойство для хранения имени исполнителя калибровки Executor.
    Добавлен публичный метод для сохранения результата SaveReport().
    Изменена толщина линий границ ячеек на Medium.

--- ReportGeneration/ReportGeneration.cs
+++ ReportGeneration/ReportGeneration.cs
@@ -14,81 +14,87 @@ namespace SMTLSoftwareTools.ReportGeneration
     {
         public string SerialNumber { get; set; }
         public string CalibrationType { get; set; }
-        public ExcelPackage Package { get; set; }
+        // Кто выполнил калибровку
+        public string Executor { get; set; }
+        private ExcelPackage package = null;
 
 
-        public ReportGeneration() { }
+        public ReportGeneration()
+        {
+            Environment.SetEnvironmentVariable("EPPlusLicenseContext", "NonCommercial");
+            package = new ExcelPackage();
+        }
 
-        public ReportGeneration(string serialNumber) { SerialNumber = serialNumber; }
+        public ReportGeneration(string serialNumber)
+        {
+            Environment.SetEnvironmentVariable("EPPlusLicenseContext", "NonCommercial");
+            package = new ExcelPackage();
+            SerialNumber = serialNumber;
+        }
 
         public void ExportToExcel(params DataGridView[] dataGridViews)
         {
-            Environment.SetEnvironmentVariable("EPPlusLicenseContext", "NonCommercial");
-            using (ExcelPackage package = new ExcelPackage())
+            int worksheetIndex = 1;
+
+            // Перебор всех DataGridView и добавление их данных в файл Excel
+            foreach (DataGridView dataGridView in dataGridViews)
             {
-                int worksheetIndex = 1;
+                ExcelWorksheet worksheet;
 
-                // Перебор всех DataGridView и добавление их данных в файл Excel
-                foreach (DataGridView dataGridView in dataGridViews)
+                if (dataGridViews.Length > 1)
                 {
-                    //var worksheet = package.Workbook.Worksheets.Add("Результат калибровки" + worksheetIndex);
-                    ExcelWorksheet worksheet;
-
-                    if (dataGridViews.Length > 1)
-                    {
-                        worksheet = package.Workbook.Worksheets.Add(CalibrationType + " Канал N " + worksheetIndex.ToString());
-                    }
-                    else
-                    {
-                        worksheet = package.Workbook.Worksheets.Add(CalibrationType);
-                    }
-
-                    worksheet.Cells[1, 1].Value = "Прибор DMC-AS02 серийный номер " + SerialNumber;
-
-                    ExcelRange range = worksheet.Cells["A3:C8"];
-                    range.Style.Border.Top.Style = ExcelBorderStyle.Thin;
-                    range.Style.Border.Top.Color.SetColor(System.Drawing.Color.Black);
-                    range.Style.Border.Left.Style = ExcelBorderStyle.Thin;
-                    range.Style.Border.Left.Color.SetColor(System.Drawing.Color.Black);
-                    range.Style.Border.Right.Style = ExcelBorderStyle.Thin;
-                    range.Style.Border.Right.Color.SetColor(System.Drawing.Color.Black);
-                    range.Style.Border.Bottom.Style = ExcelBorderStyle.Thin;
-                    range.Style.Border.Bottom.Color.SetColor(System.Drawing.Color.Black);
-
-                    // Запись заголовков столбцов из DataGridView
-                    for (int i = 0; i < dataGridView.ColumnCount; i++)
-                    {
-                        worksheet.Cells[3, i + 1].Value = dataGridView.Columns[i].HeaderText;
-                    }
-
-                    // Запись данных из DataGridView
-                    for (int i = 0; i < dataGridView.RowCount; i++)
-                    {
-                        for (int j = 0; j < dataGridView.ColumnCount; j++)
-                        {
-                            worksheet.Cells[i + 4, j + 1].Value = dataGridView.Rows[i].Cells[j].Value;
-                        }
-                    }
-                    worksheet.Cells.AutoFitColumns();
-                    worksheetIndex++;
+                    worksheet = package.Workbook.Worksheets.Add(CalibrationType + " Канал N " + worksheetIndex.ToString());
+                }
+                else
+                {
+                    worksheet = package.Workbook.Worksheets.Add(CalibrationType);
                 }
 
+                worksheet.Cells[1, 1].Value = "Прибор DMC-AS02 серийный номер " + SerialNumber;
+                worksheet.Cells[10, 1].Value = "Калибровку выполнил " + Executor;
 
+                ExcelRange range = worksheet.Cells["A3:C8"];
+                range.Style.Border.Top.Style = ExcelBorderStyle.Medium;
+                range.Style.Border.Top.Color.SetColor(System.Drawing.Color.Black);
+                range.Style.Border.Left.Style = ExcelBorderStyle.Medium;
+                range.Style.Border.Left.Color.SetColor(System.Drawing.Color.Black);
+                range.Style.Border.Right.Style = ExcelBorderStyle.Medium;
+                range.Style.Border.Right.Color.SetColor(System.Drawing.Color.Black);
+                range.Style.Border.Bottom.Style = ExcelBorderStyle.Medium;
+                range.Style.Border.Bottom.Color.SetColor(System.Drawing.Color.Black);
 
-
-                // Сохранение файла
-                SaveFileDialog saveFileDialog = new SaveFileDialog
+                // Запись заголовков столбцов из DataGridView
+                for (int i = 0; i < dataGridView.ColumnCount; i++)
                 {
-                    Filter = "Excel Files|*.xlsx",
-                    Title = "Save Excel File"
-                };
-
-                if (saveFileDialog.ShowDialog() == DialogResult.OK)
-                {
-                    package.SaveAs(new FileInfo(saveFileDialog.FileName));
+                    worksheet.Cells[3, i + 1].Value = dataGridView.Columns[i].HeaderText;
                 }
+
+                // Запись данных из DataGridView
+                for (int i = 0; i < dataGridView.RowCount; i++)
+                {
+                    for (int j = 0; j < dataGridView.ColumnCount; j++)
+                    {
+                        worksheet.Cells[i + 4, j + 1].Value = dataGridView.Rows[i].Cells[j].Value;
+                    }
+                }
+                worksheet.Cells.AutoFitColumns();
+                worksheetIndex++;
+            }
+
+        }
+        public void SaveReport()
+        {
+            // Сохранение файла
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Filter = "Excel Files|*.xlsx",
+                Title = "Save Excel File"
+            };
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                package.SaveAs(new FileInfo(saveFileDialog.FileName));
             }
         }
-
     }
 }
