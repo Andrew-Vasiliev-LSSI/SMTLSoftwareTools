af6455afc3a44930a1418b91c6338faaea09ad94
andrew <vasilyev_a@rakurs.com>
2025-01-10 14:56:18 +0300

    Подгодовка к формированию отчета на одном листе Excel

--- ReportGeneration/ReportGeneration.cs
+++ ReportGeneration/ReportGeneration.cs
@@ -17,6 +17,7 @@ namespace SMTLSoftwareTools.ReportGeneration
         // Кто выполнил калибровку
         public string Executor { get; set; }
         private ExcelPackage package = null;
+        private ExcelWorksheet worksheet;
 
 
         public ReportGeneration()
@@ -39,7 +40,7 @@ namespace SMTLSoftwareTools.ReportGeneration
             // Перебор всех DataGridView и добавление их данных в файл Excel
             foreach (DataGridView dataGridView in dataGridViews)
             {
-                ExcelWorksheet worksheet;
+                //ExcelWorksheet worksheet;
 
                 if (dataGridViews.Length > 1)
                 {
@@ -53,7 +54,7 @@ namespace SMTLSoftwareTools.ReportGeneration
                 worksheet.Cells[1, 1].Value = "Прибор DMC-AS02 серийный номер " + SerialNumber;
                 worksheet.Cells[10, 1].Value = "Калибровку выполнил " + Executor;
 
-                ExcelRange range = worksheet.Cells["A3:C8"];
+                ExcelRange range = worksheet.Cells["A3:C7"];
                 range.Style.Border.Top.Style = ExcelBorderStyle.Medium;
                 range.Style.Border.Top.Color.SetColor(System.Drawing.Color.Black);
                 range.Style.Border.Left.Style = ExcelBorderStyle.Medium;
@@ -95,6 +96,40 @@ namespace SMTLSoftwareTools.ReportGeneration
             {
                 package.SaveAs(new FileInfo(saveFileDialog.FileName));
             }
+        }
+
+        public void CreateWorksheet()
+        {
+            worksheet = package.Workbook.Worksheets.Add("Результат калибровки");
+            worksheet.Cells[1, 1].Value = "Прибор DMC-AS02 серийный номер " + SerialNumber;
+            worksheet.Cells[2, 1].Value = $"Дата и время проведения калибровки:  {DateTime.Now.ToString("dd MMMM yyyy HH:mm")}";
+            worksheet.Cells[12, 1].Value = "Калибровку выполнил " + Executor;
+            worksheet.Cells[4, 1].Value = "Калибровка по напряжению";
+            worksheet.Cells[4, 5].Value = "Калибровка по току";
+            worksheet.Cells[4, 9].Value = "Аналоговый выход 1";
+            worksheet.Cells[4, 13].Value = "Аналоговый выход 2";
+            worksheet.Cells[4, 17].Value = "Аналоговый выход 3";
+            worksheet.Cells[4, 21].Value = "Аналоговый выход 4";
+            FillBorders("A5:C9");
+            FillBorders("E5:G9");
+            FillBorders("I5:K9");
+            FillBorders("M5:O9");
+            FillBorders("Q5:S9");
+            FillBorders("U5:W9");
+
+            SaveReport();
+        }
+        public void FillBorders(string rangeOfCells)
+        {
+            ExcelRange range = worksheet.Cells[rangeOfCells];
+            range.Style.Border.Top.Style = ExcelBorderStyle.Medium;
+            range.Style.Border.Top.Color.SetColor(System.Drawing.Color.Black);
+            range.Style.Border.Left.Style = ExcelBorderStyle.Medium;
+            range.Style.Border.Left.Color.SetColor(System.Drawing.Color.Black);
+            range.Style.Border.Right.Style = ExcelBorderStyle.Medium;
+            range.Style.Border.Right.Color.SetColor(System.Drawing.Color.Black);
+            range.Style.Border.Bottom.Style = ExcelBorderStyle.Medium;
+            range.Style.Border.Bottom.Color.SetColor(System.Drawing.Color.Black);
         }
     }
 }
