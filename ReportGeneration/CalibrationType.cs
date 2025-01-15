80d362ae94775bee40d6dad5e00241ebf13607a5
andrew <vasilyev_a@rakurs.com>
2025-01-15 15:42:48 +0300

    Класс с типами калибровки

---
+++ ReportGeneration/CalibrationType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMTLSoftwareTools.ReportGeneration
+{
+    public class CalibrationType
+    {
+        public static string Voltage = "Voltage";
+        public static string Current = "Current";
+        public static string AnalogOutput = "AnalogOutput";
+    }
+}
